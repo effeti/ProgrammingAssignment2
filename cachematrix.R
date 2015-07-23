ec09294c6637141f0c103c25e581d6434c43f1b2
effeti <fiorella.tonello@gmail.com>
2015-07-23 19:21:58 +0200

    Chaching the inverse of a Matrix
    Program assignemt 2 of the R programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## My functions compute the inverse of a square invertible matrix or 
+## retrive the data if the inverse has already been calculated
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates a special "vector": 
+## a list containing functions that 1. set the value of the matrix,
+## 2. get the value of the matrix (getimat), 3. set the value of the 
+## inverse matrix, 4. get the value of the inverse matrix (getimat)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	im <- NULL
+	set <- function(y) {
+		x <<- y
+		im <<- NULL
+	}
+	get <- function() x
+	setimat <- function(solve) im <<- solve
+	getimat <- function() im
+	list(set = set, get = get,
+		setimat = setimat, 
+		getimat = getimat)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve gets the inverse matrix from the cache, if this
+## is already been calculated and then its value is not null. Otherwise it
+## computes the inverse of the matrix by applying the function solve() 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        	  im <- x$getimat()
+        if(!is.null(im)) {
+                message("getting cached data")
+                return(im)
+        }
+        data <- x$get()
+        im <- solve(data, ...)  ## Return a matrix that is the inverse of 'x'
+        x$setimat(im)
+        im
 }
